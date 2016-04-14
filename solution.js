9e67b0d6a4fd3a0352547f3e044f1a7e201da12d
jeremy1998 <jlee6817@withs.org>
2016-04-14 14:39:59 +0000

    submitting

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
 /****************************************************
 1. Create an array called movieQueue with at least two Movie Names as strings
 ****************************************************/
-
+var movieQueue=["Scarface","Warriors"];
 
 
 /****************************************************
@@ -10,7 +10,10 @@
 - It should add the new movie name to the end of your movieQueue array
 - Finally it should return the movie name it just added
 ****************************************************/
-
+function addMovieToEnd(paidandfull){
+  movieQueue.push(paidandfull)
+  return paidandfull;
+}
 
 
 /****************************************************
@@ -19,7 +22,10 @@
 - It should add the new movie name to the front of your movieQueue array
 - Finally it should return the movie name it just added
 ****************************************************/
-
+function addMovieToFront(spiderman){
+  movieQueue.unshift(spiderman);
+  return spiderman;
+}
 
 
 /****************************************************
@@ -29,7 +35,10 @@
 - It should add the new movie name to the movieQueue array at the given index.
 - Finally it should return the movie name it just added
 ****************************************************/
-
+function changeMovie(index,getrichordietrying){
+  movieQueue[index]=getrichordietrying
+  return getrichordietrying;
+}
 
 
 /****************************************************
@@ -40,5 +49,7 @@
 - If the index is not valid (meaning that it's larger than the number of items in the array),
   it should return the string "not a valid index"
 ****************************************************/
+function getMovie(one){
 
-
+  return movieQueue[one];
+}
